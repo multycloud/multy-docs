734e3b1836529ea3fb05d0e0567cb113805af09a
Joao Coelho <joao.msc.coelho@gmail.com>
2022-04-16 17:12:09 +0100

    hide blogs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -108,7 +108,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Multy.`,
       },
       announcementBar: {
         id: "announcementBar-2", // Increment on change
